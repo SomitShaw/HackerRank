e78a0d93292445bb3c50364967b1501252bed5e4
Somit Shaw <somitshaw1999@gmail.com>
2023-10-01 02:51:46 +0530

    Commiting main file

--- myArray.java
+++ myArray.java
@@ -42,8 +42,8 @@ public class myArray {
     }
 
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
-        myArray first = new myArray("Somit", 7, arr, "Y", 3);
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        myArray first = new myArray("Somit", 8, arr, "Y", 3);
         System.out.println(first.toString());
     }
 }
